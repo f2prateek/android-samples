b6c9104c2e39a123d18d784be8a4156ca873e831
Prateek Srivastava <f2prateek@gmail.com>
2013-04-16 12:02:25 -0600

    Cleanup the MainActivity

--- src/main/java/com/f2prateek/MainActivity.java
+++ src/main/java/com/f2prateek/MainActivity.java
@@ -1,24 +1,33 @@
 package com.f2prateek;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
-    private static String TAG = "HelloAndroidActivity";
+    private static final String TAG = "HelloAndroidActivity";
+    private static final String[] SAMPLE_LIST = {"ListView Recycling"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SAMPLE_LIST));
     }
 
-    public void startRecyclingListActivity(View v) {
-        Intent i = new Intent(this, RecyclingListActivity.class);
-        startActivity(i);
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent;
+        switch (position) {
+            default:
+            case 0:
+                intent = new Intent(this, RecyclingListActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 
 }
