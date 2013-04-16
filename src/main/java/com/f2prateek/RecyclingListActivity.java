62f1df347de5f68730741968f75bc375b075ff59
Prateek Srivastava <f2prateek@gmail.com>
2013-04-16 11:59:07 -0600

    Cleanup the sample.

--- src/main/java/com/f2prateek/RecyclingListActivity.java
+++ src/main/java/com/f2prateek/RecyclingListActivity.java
@@ -1,37 +1,31 @@
 package com.f2prateek;
 
-import android.app.Activity;
-import android.content.Context;
+import android.app.ListActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 /**
  * An example to observe how ListView recycling works.
  */
-public class RecyclingListActivity extends Activity {
+public class RecyclingListActivity extends ListActivity {
 
+    private static final int SAMPLE_SIZE = 50;
     private static String TAG = "RecyclingListActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recycling_list_activity);
-        ListView mListView = (ListView) findViewById(android.R.id.list);
-        mListView.setAdapter(new SimpleTextAdapter(this, 40));
+        setListAdapter(new SimpleTextAdapter(SAMPLE_SIZE));
     }
 
     private class SimpleTextAdapter extends BaseAdapter {
         private final int count;
-        private final Context context;
 
-        private SimpleTextAdapter(Context context, int count) {
-            this.context = context;
+        private SimpleTextAdapter(int count) {
             this.count = count;
         }
 
@@ -48,15 +42,21 @@ public class RecyclingListActivity extends Activity {
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView;
+            String text;
+            Drawable drawable;
             if (convertView == null) {
-                convertView = LayoutInflater.from(context).inflate(R.layout.recycling_list_item, parent, false);
-                ((TextView) convertView.findViewById(R.id.textView)).setText(context.getString(R.string.unrecycled_text, position));
-                ((ImageView) convertView.findViewById(R.id.imageView)).setImageResource(R.drawable.ic_action_unrecycled);
+                textView = new TextView(RecyclingListActivity.this);
+                text = getResources().getString(R.string.unrecycled_text, position);
+                drawable = getResources().getDrawable(R.drawable.ic_action_unrecycled);
             } else {
-                ((TextView) convertView.findViewById(R.id.textView)).setText(context.getString(R.string.recycled_text, position));
-                ((ImageView) convertView.findViewById(R.id.imageView)).setImageResource(R.drawable.ic_action_recycled);
+                textView = (TextView) convertView;
+                text = getResources().getString(R.string.recycled_text, position);
+                drawable = getResources().getDrawable(R.drawable.ic_action_recycled);
             }
-            return convertView;
+            textView.setText(text);
+            textView.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+            return textView;
         }
     }
 
